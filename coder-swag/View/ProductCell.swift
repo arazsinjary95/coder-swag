7d6e66789e29f11668878e7e195bca6384e7bb28
Araz Sinjary <araz@Arazs-MBP.Home>
2018-05-12 17:51:25 +0300

    collectionview layout

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Araz Sinjary on 5/12/18.
+//  Copyright © 2018 Araz Sinjary. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
